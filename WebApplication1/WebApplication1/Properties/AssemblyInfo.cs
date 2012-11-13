84c9ae781fd7f013ca2651b5f0608e9ea55cad52
apropoapo <apropoapo@hotmail.de>
2012-11-13 13:23:19 +0100

    0.1.4

--- WebApplication1/WebApplication1/Properties/AssemblyInfo.cs
+++ WebApplication1/WebApplication1/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("myWebCloudServiceProject")]
+[assembly: AssemblyTitle("WebApplication1")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("myWebCloudServiceProject")]
+[assembly: AssemblyProduct("WebApplication1")]
 [assembly: AssemblyCopyright("Copyright ©  2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("cb79629a-9d07-4710-a0c6-462bad090043")]
+[assembly: Guid("0529fd5d-a704-4469-8779-a777ea7120f2")]
 
 // Version information for an assembly consists of the following four values:
 //
