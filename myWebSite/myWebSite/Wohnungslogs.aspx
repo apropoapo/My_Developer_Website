cbc8f4ecc7e5c1312056bcecacca522d654793b9
apropoapo <apropoapo@hotmail.de>
2013-03-12 18:12:46 +0100

    0.3.4 - Wohnungslog 1.1

--- myWebSite/myWebSite/Wohnungslogs.aspx
+++ myWebSite/myWebSite/Wohnungslogs.aspx
@@ -7,57 +7,100 @@
     <title>Wohnungslog</title>
 </head>
 <body>
-<h1>Wohnungslogs</h1>
     <form id="form1" runat="server">
+    <h1>
+        Wohnungslogs - Version 1.1</h1>
+    <h2>
+        Changelog: 
+        <br />
+        Version 1.1: die aktuellsten 40 Wohnungen werden (absteigend sortiert) angezeigt:
+        450-850€ Kaltmiete. 2-3 Zimmer. ganz München. Jede Minute wird überprüft ob neue Wohnung -> Wenn ja, dann wird Alarm ausgelöst.</h2>
+    <asp:ScriptManager ID="ScriptManager1" runat="server">
+    </asp:ScriptManager>
+        <asp:Timer ID="Timer1" runat="server" ontick="Timer1_Tick">
+    </asp:Timer>
+    <asp:Label ID="lbl1" runat="server" Text="Alarmverzeichnis: "></asp:Label>
     <div>
         <asp:DataList ID="myDatalst" runat="server" CellPadding="0" CellSpacing="0" Width="100%">
             <ItemTemplate>
                 <tr>
                     <td>
-                    <a  href='<%# DataBinder.Eval(Container.DataItem, "Link")%>'>
-                        <div id="line" />
-                        <div id="header">
-                            <%# DataBinder.Eval(Container.DataItem, "Header")%></div>
-                        <asp:Image ID="Bild" runat="server" ImageUrl='<%# DataBinder.Eval(Container.DataItem, "Bild")%>' />
-                        <table id="table" align="center" frame="void" >
-                            <tr class="contentTableRow">
-                                <td>Kaltemiete:</td>
-                                <td><%# DataBinder.Eval(Container.DataItem, "Kaltmiete")%></td>
-                            </tr>
-                            <tr class="contentTableRow">
-                                <td>Wohnflaeche:</td>
-                                <td><%# DataBinder.Eval(Container.DataItem, "Wohnflaeche")%></td>
-                            </tr>
-                            <tr class="contentTableRow">
-                                <td>Zimmer:</td>
-                                <td><%# DataBinder.Eval(Container.DataItem, "Zimmer")%></td>      
-                            </tr>
-                            <tr class="contentTableRow">
-                                <td>Ausstattung:</td>
-                                <td><%# DataBinder.Eval(Container.DataItem, "Ausstattung1")%></td>
-                            </tr>
-                            <tr>
-                                <td></td>
-                                <td><%# DataBinder.Eval(Container.DataItem, "Ausstattung2")%></td>
-                            </tr>
-                            <tr class="contentTableRow">
-                                <td></td>
-                                <td><%# DataBinder.Eval(Container.DataItem, "Ausstattung3")%></td>
-                            </tr>
-                            <tr class="contentTableRow">
-                                <td></td>
-                                <td><%# DataBinder.Eval(Container.DataItem, "Ausstattung4")%></td>
-                            </tr>
-                            <tr class="contentTableRow">
-                                <td>Lage:</td>
-                                <td><%# DataBinder.Eval(Container.DataItem, "Lage")%></td>
-                            </tr>
-                            <tr class="contentTableRow">
-                                <td>Anzeige seit:</td>
-                                <td><%# DataBinder.Eval(Container.DataItem, "Zeit")%></td>
-                            </tr>
- </a>
-                        </table>
+                        <a href='<%# DataBinder.Eval(Container.DataItem, "Link")%>'>
+                            <div id="line" />
+                            <div id="header">
+                                <%# DataBinder.Eval(Container.DataItem, "Header")%></div>
+                            <asp:Image ID="Bild" runat="server" ImageUrl='<%# DataBinder.Eval(Container.DataItem, "Bild")%>' />
+                            <table id="table" align="center" frame="void">
+                                <tr class="contentTableRow">
+                                    <td>
+                                        Kaltemiete:
+                                    </td>
+                                    <td>
+                                        <%# DataBinder.Eval(Container.DataItem, "Kaltmiete")%>
+                                    </td>
+                                </tr>
+                                <tr class="contentTableRow">
+                                    <td>
+                                        Wohnflaeche:
+                                    </td>
+                                    <td>
+                                        <%# DataBinder.Eval(Container.DataItem, "Wohnflaeche")%>
+                                    </td>
+                                </tr>
+                                <tr class="contentTableRow">
+                                    <td>
+                                        Zimmer:
+                                    </td>
+                                    <td>
+                                        <%# DataBinder.Eval(Container.DataItem, "Zimmer")%>
+                                    </td>
+                                </tr>
+                                <tr class="contentTableRow">
+                                    <td>
+                                        Ausstattung:
+                                    </td>
+                                    <td>
+                                        <%# DataBinder.Eval(Container.DataItem, "Ausstattung1")%>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                    </td>
+                                    <td>
+                                        <%# DataBinder.Eval(Container.DataItem, "Ausstattung2")%>
+                                    </td>
+                                </tr>
+                                <tr class="contentTableRow">
+                                    <td>
+                                    </td>
+                                    <td>
+                                        <%# DataBinder.Eval(Container.DataItem, "Ausstattung3")%>
+                                    </td>
+                                </tr>
+                                <tr class="contentTableRow">
+                                    <td>
+                                    </td>
+                                    <td>
+                                        <%# DataBinder.Eval(Container.DataItem, "Ausstattung4")%>
+                                    </td>
+                                </tr>
+                                <tr class="contentTableRow">
+                                    <td>
+                                        Lage:
+                                    </td>
+                                    <td>
+                                        <%# DataBinder.Eval(Container.DataItem, "Lage")%>
+                                    </td>
+                                </tr>
+                                <tr class="contentTableRow">
+                                    <td>
+                                        Anzeige seit:
+                                    </td>
+                                    <td>
+                                        <%# DataBinder.Eval(Container.DataItem, "Zeit")%>
+                                    </td>
+                                </tr>
+                        </a></table>
                     </td>
                 </tr>
             </ItemTemplate>
--- myWebSite/myWebSite/Wohnungslogs.aspx.cs
+++ myWebSite/myWebSite/Wohnungslogs.aspx.cs
@@ -13,88 +13,16 @@ namespace myWebSite
     public partial class Wohnungslogs : System.Web.UI.Page
     {
 
-          const string CONSTRING = "Server=instance29437.db.xeround.com;Port=19153;Database=users;Uid=appharbor;Pwd=NNDKjRzh";
+        DateTime aktuell_alt = new DateTime(0);
+        DateTime aktuell_neu = new DateTime(0);
+        bool initial = true;
+        const string CONSTRING = "Server=instance29437.db.xeround.com;Port=19153;Database=users;Uid=appharbor;Pwd=NNDKjRzh";
 
         protected void Page_Load(object sender, EventArgs e)
         {
-            // Create sample data for the DataList control.
-            DataTable dt = new DataTable();
-            DataRow dr;
+            Timer1.Enabled = true;
 
-            // Define the columns of the table.
-
-            dt.Columns.Add(new DataColumn("Link", typeof(String)));
-            dt.Columns.Add(new DataColumn("Header", typeof(String)));
-            dt.Columns.Add(new DataColumn("Bild", typeof(String)));
-            dt.Columns.Add(new DataColumn("Kaltmiete", typeof(String)));
-            dt.Columns.Add(new DataColumn("Wohnflaeche", typeof(String)));
-            dt.Columns.Add(new DataColumn("Zimmer", typeof(String)));
-            dt.Columns.Add(new DataColumn("Ausstattung1", typeof(String)));
-            dt.Columns.Add(new DataColumn("Ausstattung2", typeof(String)));
-            dt.Columns.Add(new DataColumn("Ausstattung3", typeof(String)));
-            dt.Columns.Add(new DataColumn("Ausstattung4", typeof(String)));
-            dt.Columns.Add(new DataColumn("Lage", typeof(String)));
-            dt.Columns.Add(new DataColumn("Zeit", typeof(String)));
-            
-            //my_init();
-            HashSet<int> currentIDs = new HashSet<int>();
-
-            //connect
-            MySqlConnection con = new MySqlConnection(CONSTRING);
-            con.Open();
-
-
-            //adapter
-            MySqlDataAdapter adapter = new MySqlDataAdapter();
-            //SQL Abfrage erstellen
-            string cmdText = "SELECT * FROM Web_Whg_table ORDER BY ZEIT DESC";
-            MySqlCommand cmd = new MySqlCommand(cmdText, con);
-
-            //Datatable abrufen
-            DataTable dt2 = new DataTable();
-            adapter.SelectCommand = cmd;
-            adapter.Fill(dt2);
-            DataRowCollection dataRowC = dt2.Rows;
-
-
-            int j= dataRowC.Count;
-
-            for (int i = 0; i < j; i++)
-            {
-               // ListBox1.Items.Add("Zeit: " + dataRowC[i].ItemArray[11] + " ||| ID: " + dataRowC[i].ItemArray[0] + "   |||    Header: " + dataRowC[i].ItemArray[1] + "    |||   Miete: " + dataRowC[i].ItemArray[3] + "   |||    Zimmer: " + dataRowC[i].ItemArray[4] + "   |||    Flaeche: " + dataRowC[i].ItemArray[5] + "  |||     Lage: " + dataRowC[i].ItemArray[10]);
-                dr = dt.NewRow();
-
-                dr[0] = "http://www.immobilienscout24.de/expose/" + dataRowC[i].ItemArray[0];   // Header
-                dr[1] = dataRowC[i].ItemArray[1];   // Header
-                dr[2] = dataRowC[i].ItemArray[2];   // Picture
-                dr[3] = dataRowC[i].ItemArray[3];   //Kaltmiete
-                dr[4] = dataRowC[i].ItemArray[5];   //Wohnfläche
-                dr[5] = dataRowC[i].ItemArray[4];   //Zimmer
-                dr[6] = dataRowC[i].ItemArray[6];   //Ausstattung1
-                dr[7] = dataRowC[i].ItemArray[7];   //Ausstattung2
-                dr[8] = dataRowC[i].ItemArray[8];   //Ausstattung3
-                dr[9] = dataRowC[i].ItemArray[9];   //Ausstattung4
-                dr[10] = dataRowC[i].ItemArray[10];   //Lage
-                dr[11] = dataRowC[i].ItemArray[11];   //Zeit
-
-
-                dt.Rows.Add(dr);
-
-            }
-
-            DataView dv = new DataView(dt);
-
-            myDatalst.DataSource = dv;
-            //    = new[] {
-            //       new { Bild = "http://picture.immobilienscout24.de/files/odw002/N/176/943/76/176943076-3.jpg?1174550284"},
-            //      new { Bild = "http://picture.immobilienscout24.de/files/odw002/N/176/943/76/176943076-3.jpg?1174550284"}
-
-            //};
-            
-
-            myDatalst.DataBind();
-
-            con.Close();
+            loadData(initial);
             
         }
 
@@ -129,6 +57,121 @@ namespace myWebSite
 
             DataView dv = new DataView(dt);
             return dv;
+        }
+
+        protected void loadData(bool init)
+        {
+
+          
+
+            // Create sample data for the DataList control.
+            DataTable dt = new DataTable();
+            DataRow dr;
+
+            // Define the columns of the table.
+
+            dt.Columns.Add(new DataColumn("Link", typeof(String)));
+            dt.Columns.Add(new DataColumn("Header", typeof(String)));
+            dt.Columns.Add(new DataColumn("Bild", typeof(String)));
+            dt.Columns.Add(new DataColumn("Kaltmiete", typeof(String)));
+            dt.Columns.Add(new DataColumn("Wohnflaeche", typeof(String)));
+            dt.Columns.Add(new DataColumn("Zimmer", typeof(String)));
+            dt.Columns.Add(new DataColumn("Ausstattung1", typeof(String)));
+            dt.Columns.Add(new DataColumn("Ausstattung2", typeof(String)));
+            dt.Columns.Add(new DataColumn("Ausstattung3", typeof(String)));
+            dt.Columns.Add(new DataColumn("Ausstattung4", typeof(String)));
+            dt.Columns.Add(new DataColumn("Lage", typeof(String)));
+            dt.Columns.Add(new DataColumn("Zeit", typeof(DateTime)));
+
+            //my_init();
+            HashSet<int> currentIDs = new HashSet<int>();
+
+            //connect
+            MySqlConnection con = new MySqlConnection(CONSTRING);
+            con.Open();
+
+
+            //adapter
+            MySqlDataAdapter adapter = new MySqlDataAdapter();
+            //SQL Abfrage erstellen
+            string cmdText = "SELECT * FROM Web_Whg_table ORDER BY ZEIT DESC";
+            MySqlCommand cmd = new MySqlCommand(cmdText, con);
+
+            //Datatable abrufen
+            DataTable dt2 = new DataTable();
+            adapter.SelectCommand = cmd;
+            adapter.Fill(dt2);
+            DataRowCollection dataRowC = dt2.Rows;
+
+
+            int j = dataRowC.Count;
+
+
+            
+            // alarm
+            aktuell_alt = aktuell_neu;
+            aktuell_neu = ((System.DateTime)dataRowC[0].ItemArray[11]).AddHours(5.0); 
+
+            if (j > 20)
+            {
+                j = 50;
+            }
+
+            if (!init)
+            {
+                if (aktuell_neu>aktuell_alt)
+                {
+                    System.Media.SystemSounds.Beep.Play();
+                    lbl1.Text = " Alarm ausgelöst um:" + aktuell_neu + " ||";
+                }
+            }
+
+            for (int i = 0; i < j; i++)
+            {
+                // ListBox1.Items.Add("Zeit: " + dataRowC[i].ItemArray[11] + " ||| ID: " + dataRowC[i].ItemArray[0] + "   |||    Header: " + dataRowC[i].ItemArray[1] + "    |||   Miete: " + dataRowC[i].ItemArray[3] + "   |||    Zimmer: " + dataRowC[i].ItemArray[4] + "   |||    Flaeche: " + dataRowC[i].ItemArray[5] + "  |||     Lage: " + dataRowC[i].ItemArray[10]);
+                dr = dt.NewRow();
+
+                dr[0] = "http://www.immobilienscout24.de/expose/" + dataRowC[i].ItemArray[0];   // Header
+                dr[1] = dataRowC[i].ItemArray[1];   // Header
+                dr[2] = dataRowC[i].ItemArray[2];   // Picture
+                dr[3] = dataRowC[i].ItemArray[3];   //Kaltmiete
+                dr[4] = dataRowC[i].ItemArray[5];   //Wohnfläche
+                dr[5] = dataRowC[i].ItemArray[4];   //Zimmer
+                dr[6] = dataRowC[i].ItemArray[6];   //Ausstattung1
+                dr[7] = dataRowC[i].ItemArray[7];   //Ausstattung2
+                dr[8] = dataRowC[i].ItemArray[8];   //Ausstattung3
+                dr[9] = dataRowC[i].ItemArray[9];   //Ausstattung4
+                dr[10] = dataRowC[i].ItemArray[10];   //Lage
+                dr[11] = ((System.DateTime)dataRowC[i].ItemArray[11]).AddHours(5.0);   //Zeit
+
+
+                dt.Rows.Add(dr);
+
+            }
+
+            DataView dv = new DataView(dt);
+
+            myDatalst.DataSource = dv;
+            //    = new[] {
+            //       new { Bild = "http://picture.immobilienscout24.de/files/odw002/N/176/943/76/176943076-3.jpg?1174550284"},
+            //      new { Bild = "http://picture.immobilienscout24.de/files/odw002/N/176/943/76/176943076-3.jpg?1174550284"}
+
+            //};
+
+
+            myDatalst.DataBind();
+
+            con.Close();
+
+            if (!init)
+                initial = false;
+            
+
+        }
+
+        protected void Timer1_Tick(object sender, EventArgs e)
+        {
+            loadData(initial);
         }
 
     }
